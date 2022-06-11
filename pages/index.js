0cb17e3e2fccffd5982876c535e3e842cbf13fa9
satou1497 <satou1497@gmail.com>
2022-06-11 10:07:08 +0900

    220611

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <p>satou</p>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
