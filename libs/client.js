aa955996398859118a428862a3747bc025641e69
satou1497 <satou1497@gmail.com>
2022-06-12 15:40:24 +0900

    どこを変えたか不明

--- libs/client.js
+++ libs/client.js
@@ -4,5 +4,5 @@ import { createClient } from 'microcms-js-sdk';
 export const client = createClient({
   serviceDomain: 'kfbk',
   // apiKey: process.env.API_KEY, // 次の定義にするとVercelでの設定は不要
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  apiKey: process.env.API_KEY,
 });
